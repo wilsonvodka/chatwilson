1083bbe6996c9f1a4251bf73687a366c261cf2e0
Alexander <wilgm0kdev@gmail.com>
2022-02-12 20:04:11 -0500

    correccion de app

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.get("*", function (req, res) {
   res.render("error404");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 4000, function () {
   console.log("Server started on port 3000");
 });
